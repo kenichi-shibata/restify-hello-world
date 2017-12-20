aa583e52de3b3756e4289eece66ed3fdeb02f63a
Kenichi Shibata <kenichi-shibata@outlook.com>
2017-12-20 18:43:06 +0900

    server: Enabled CORS anywhere use 80 http standard port

--- server.js
+++ server.js
@@ -6,9 +6,33 @@ res.send('hello '+ req.params.name);
 
 var server = restify.createServer();
 
+server.use(
+  function crossOrigin(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    return next();
+  }
+);
+
+server.opts(/.*/, function (req,res,next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", req.header("Access-Control-Request-Method"));
+    res.header("Access-Control-Allow-Headers", req.header("Access-Control-Request-Headers"));
+    res.send(200);
+    return next();
+});
+
+server.get('/test', function (req,res,next) {
+
+    res.send({
+        status: "ok"
+    });
+    return next();
+});
+
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond); 
 
-server.listen(8080, function() {
+server.listen(80, function() {
  console.log('%s listening at %s', server.name, server.url);
 });
